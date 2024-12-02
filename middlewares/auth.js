d560f2ca667e0ad069083bdda6b45f174093e066
irshad <irshadmglm@gmail.com>
2024-12-02 23:48:28 +0530

    auth updated

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,9 +5,10 @@ const db = require('../config/connection');
 const collections = require('../config/collection');
 const bcrypt = require('bcrypt');
 const { ObjectId } = require('mongodb');
-console.log(process.env.GOOGLE_CLIENT_ID);
-console.log(process.env.GOOGLE_CLIENT_SECRET);
-console.log(process.env.GOOGLE_CALLBACK_URL);
+console.log('GOOGLE_CLIENT_ID:', process.env.GOOGLE_CLIENT_ID);
+console.log('GOOGLE_CLIENT_SECRET:', process.env.GOOGLE_CLIENT_SECRET);
+console.log('GOOGLE_CALLBACK_URL:', process.env.GOOGLE_CALLBACK_URL);
+
 
 // Configure Google OAuth Strategy
 passport.use(new GoogleStrategy({
